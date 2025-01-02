b42e54669dfbae63719425a3ab820c3fcfc7fbbb
ila1020 <ilaria.magni@mail.polimi.it>
2025-01-02 21:30:01 +0100

    impostazione homepage 01

--- sketch.js
+++ sketch.js
@@ -5,8 +5,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
+
   textAlign(CENTER, CENTER);
   textSize(16);
   text(message, width / 2, height / 2);
